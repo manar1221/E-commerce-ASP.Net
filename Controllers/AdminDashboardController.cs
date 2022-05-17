d533d7a1a387f5f54afafdfd02c14e425a9fdd32
Mostafa-Mohamed18 <81434651+Mostafa-Mohamed18@users.noreply.github.com>
2022-05-17 21:02:38 +0200

    Controllers

---
+++ Controllers/AdminDashboardController.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using E_Shopping.DB;
+using E_Shopping.Models;
+using E_Shopping.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace E_Shopping.Controllers
+{
+    public class AdminDashboardController : Controller
+    {
+        // GET: AdminDashboard
+
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+
+        public List<SelectListItem> GetCategory()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            var cat = _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecords();
+            foreach (var item in cat)
+            {
+                list.Add(new SelectListItem { Value = item.CategoryId.ToString(), Text = item.CategoryName });
+            }
+            return list;
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult Categories()
+        {
+
+            return View(_unitOfWork.GetRepositoryInstance<Tbl_Category>().GetCategory());
+        }
+        public ActionResult CategoryAdd()
+        {
+            ViewBag.CategoryList = GetCategory();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CategoryAdd(Tbl_Category Category)
+        {
+            _unitOfWork.GetRepositoryInstance<Tbl_Category>().Add(Category);
+            return RedirectToAction("Categories");
+        }
+
+        public ActionResult CategoryEdit(int catId)
+        {
+            return View(_unitOfWork.GetRepositoryInstance<Tbl_Category>().GetFirstorDefault(catId));
+        }
+        [HttpPost]
+        public ActionResult CategoryEdit(Tbl_Category tbl)
+        {
+            _unitOfWork.GetRepositoryInstance<Tbl_Category>().Update(tbl);
+            return RedirectToAction("Categories");
+        }
+        public ActionResult Product()
+        {
+            return View(_unitOfWork.GetRepositoryInstance<Tbl_Product>().GetProduct());
+        }
+        public ActionResult ProductEdit(int productId)
+        {
+            ViewBag.CategoryList = GetCategory();
+            return View(_unitOfWork.GetRepositoryInstance<Tbl_Product>().GetFirstorDefault(productId));
+        }
+        [HttpPost]
+        public ActionResult ProductEdit(Tbl_Product tbl, HttpPostedFileBase file)
+        {
+            string pic=null;
+            if (file != null)
+            {
+                pic = System.IO.Path.GetFileName(file.FileName);
+                string path = System.IO.Path.Combine(Server.MapPath("~/ProductImg/"), pic);
+                // file is uploaded
+                file.SaveAs(path);
+            }
+            tbl.ProductImage = file != null ? pic : tbl.ProductImage;
+            tbl.ModifiedDate = DateTime.Now;
+            _unitOfWork.GetRepositoryInstance<Tbl_Product>().Update(tbl);
+            return RedirectToAction("Product");
+        }
+        public ActionResult ProductAdd()
+        {
+            ViewBag.CategoryList = GetCategory();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ProductAdd(Tbl_Product Product, HttpPostedFileBase file)
+        {
+            string pic = null;
+            if (file != null)
+            {
+                pic = System.IO.Path.GetFileName(file.FileName);
+                string path = System.IO.Path.Combine(Server.MapPath("~/ProductImg/"), pic);
+                // file is uploaded
+                file.SaveAs(path);
+            }
+            Product.ProductImage = pic;
+            Product.CreatedDate = DateTime.Now;
+            _unitOfWork.GetRepositoryInstance<Tbl_Product>().Add(Product);
+            return RedirectToAction("Product");
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,123 @@
+﻿using Newtonsoft.Json;
+using E_Shopping.DB;
+using E_Shopping.Models;
+using E_Shopping.Models.Home;
+using E_Shopping.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace E_Shopping.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbMyShopEntities dbContext = new dbMyShopEntities();
+        public ActionResult Index()
+        {
+            HomePageViewModel model = new HomePageViewModel();
+            return View(model.CreateModel());
+        }
+
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+
+        public ActionResult CheckoutDetails()
+        {
+            return View();
+        }
+        public ActionResult DecreaseQty(int productId)
+        {
+            if (Session["cart"] != null)
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                var product = dbContext.Tbl_Product.Find(productId);
+                foreach (var item in cart)
+                {
+                    if (item.Product.ProductId == productId)
+                    {
+                        int prevQty = item.Quantity;
+                        if (prevQty > 0)
+                        {
+                            cart.Remove(item);
+                            cart.Add(new Item()
+                            {
+                                Product = product,
+                                Quantity = prevQty - 1
+                            });
+                        }
+                        break;
+                    }
+                }
+                Session["cart"] = cart;
+            }
+            return Redirect("Checkout");
+        }
+        public ActionResult AddToCart(int productId, string url)
+        {
+            if (Session["cart"] == null)
+            {
+                List<Item> cart = new List<Item>();
+                var product = dbContext.Tbl_Product.Find(productId);
+                cart.Add(new Item()
+                {
+                    Product = product,
+                    Quantity = 1
+                });
+                Session["cart"] = cart;
+            }
+            else
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                var count = cart.Count();
+                var product = dbContext.Tbl_Product.Find(productId);
+                for (int i = 0; i < count; i++)
+                {
+                    if (cart[i].Product.ProductId == productId)
+                    {
+                        int prevQty = cart[i].Quantity;
+                        cart.Remove(cart[i]);
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = prevQty + 1
+                        });
+                        break;
+                    }
+                    else
+                    {
+                        var prd = cart.Where(x => x.Product.ProductId == productId).SingleOrDefault();
+                        if (prd == null)
+                        {
+                            cart.Add(new Item()
+                            {
+                                Product = product,
+                                Quantity = 1
+                            });
+                        }
+                    }
+                }
+                Session["cart"] = cart;
+            }
+            return Redirect(url);
+        }
+        public ActionResult RemoveFromCart(int productId)
+        {
+            List<Item> cart = (List<Item>)Session["cart"];
+            foreach (var item in cart)
+            {
+                if (item.Product.ProductId == productId)
+                {
+                    cart.Remove(item);
+                    break;
+                }
+            }
+            Session["cart"] = cart;
+            return Redirect("Index");
+        }
+    }
+}
