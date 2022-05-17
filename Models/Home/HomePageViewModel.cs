4a314591b83bfeff25e749109e93c5bafec10a4a
Mostafa-Mohamed18 <81434651+Mostafa-Mohamed18@users.noreply.github.com>
2022-05-17 20:40:37 +0200

    Db-Models

---
+++ Models/Home/HomePageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using E_Shopping.DB;
+using E_Shopping.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Shopping.Models.Home
+{
+    public class HomePageViewModel
+    {
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+        public IEnumerable<Tbl_Product> ListOfProducts { get; set; }
+        public HomePageViewModel CreateModel()
+        {
+            return new HomePageViewModel
+            {
+                ListOfProducts = _unitOfWork.GetRepositoryInstance<Tbl_Product>().GetAllRecords()
+        };
+        }
+
+    }
+}
---
+++ Models/Home/Item.cs
@@ -0,0 +1,14 @@
+﻿using E_Shopping.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Shopping.Models.Home
+{
+    public class Item
+    {
+        public Tbl_Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
