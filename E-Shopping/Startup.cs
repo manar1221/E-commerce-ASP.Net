0b792c748748a620f868a292debfb441cdc46ced
Mostafa-Mohamed18 <81434651+Mostafa-Mohamed18@users.noreply.github.com>
2022-05-17 20:54:15 +0200

    Packages

---
+++ E-Shopping/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_Shopping.Startup))]
+namespace E_Shopping
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
